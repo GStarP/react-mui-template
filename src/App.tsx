1a75a55fa0cc937930cff9a8cacf05d370c1ce9a
GStarP <1700066935@qq.com>
2022-04-29 22:51:31 +0800

    chore: react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import Main from './pages/Main'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>APP</h1>
+      <div>
+        <Link to="/">To Main</Link>
+        <Link to="/about">To About</Link>
+      </div>
+      <Routes>
+        <Route index element={<Main />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
