977a68b06bf8914953387e980b40c07cd5d76f71
GStarP <1700066935@qq.com>
2022-04-29 23:11:32 +0800

    chore(UI): Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './App.css'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 import Main from './pages/Main/Main'
 import About from './pages/About/About'
+import MButton from '@mui/material/Button'
 
 function App() {
+  const nav = useNavigate()
   return (
     <div className="app">
       <h1>APP</h1>
       <div>
-        <Link to="/">To Main</Link>
+        <MButton
+          variant="contained"
+          onClick={() => {
+            nav('/')
+          }}
+        >
+          To Main
+        </MButton>
         <Link to="/about">To About</Link>
       </div>
       <Routes>
